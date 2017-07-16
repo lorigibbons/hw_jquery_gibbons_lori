6ad2fee2f4a5473d9e1d1e0465a2de230ddadc05
Lori Gibbons <lori.j.gibbons@gmail.com>
2017-07-16 15:07:22 -0400

    Change css properties with JQ

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    var paragraphs = $('p');
+    // var newStyle = function () {
+    //     paragraphs.css("color", "red");
+    // }
+    $(paragraphs).css({
+        'background-color': 'aqua',
+        'font-size': '1.4rem',
+        'color': 'blue',
+    });
+});
